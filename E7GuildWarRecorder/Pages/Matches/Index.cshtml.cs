de0e36dc09d5483a3e4170bea1b78441d9b67972
Jim <projectjimcs@gmail.com>
2022-07-04 18:54:32 -0700

    Details page search functionality basics

--- E7GuildWarRecorder/Pages/Matches/Index.cshtml.cs
+++ E7GuildWarRecorder/Pages/Matches/Index.cshtml.cs
@@ -6,6 +6,7 @@ using E7GuildWarRecorder.Database;
 using E7GuildWarRecorder.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace E7GuildWarRecorder.Pages.Matches
 {
@@ -14,20 +15,44 @@ namespace E7GuildWarRecorder.Pages.Matches
         private readonly ApplicationDbContext _db;
         public List<Match>? Matches { get; set; }
         public List<Hero>? Heroes { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int Defense1Id { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int Defense2Id { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int Defense3Id { get; set; }
 
         public IndexModel(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        public void OnGet(int defense1Id)
+        public void OnGet()
         {
-            Console.WriteLine(defense1Id);
             Heroes = _db.Hero
             .OrderBy(hero => hero.Name)
             .ToList();
 
-            Matches = _db.Match.ToList();
+            Console.WriteLine(Defense1Id);
+            IQueryable<Match> query = _db.Match.Select(m => m);
+
+            // !!! Refactor
+            if (Defense1Id != 0)
+            {
+                query = query.Where(m => Defense1Id == m.Defense1Id || Defense1Id == m.Defense2Id || Defense1Id == m.Defense3Id);
+            }
+
+            if (Defense2Id != 0)
+            {
+                query = query.Where(m => Defense2Id == m.Defense1Id || Defense2Id == m.Defense2Id || Defense2Id == m.Defense3Id);
+            }
+
+            if (Defense3Id != 0)
+            {
+                query = query.Where(m => Defense3Id == m.Defense1Id || Defense3Id == m.Defense2Id || Defense3Id == m.Defense3Id);
+            }
+
+            Matches = query.ToList();
         }
     }
 }
