a983d921772c663890e4fe6c3e521d5c49a75921
Jim <projectjimcs@gmail.com>
2022-07-18 19:36:26 -0700

    Added initial logic for dashboard defenses logic

--- E7GuildWarRecorder/Pages/Index.cshtml.cs
+++ E7GuildWarRecorder/Pages/Index.cshtml.cs
@@ -8,10 +8,15 @@ namespace E7GuildWarRecorder.Pages;
 
 public class IndexModel : PageModel
 {
+    const int withinDays = -30;
+    const int maxMatchesToShow = 10;
+
     private readonly ILogger<IndexModel> _logger;
     private readonly ApplicationDbContext _db;
     public List<Match>? Matches { get; set; }
     public Dictionary<string, List<Match>> MatchesByDefenseIds { get; set; }
+    public Dictionary<string, Dictionary<string, float>> MatchStatsByIds { get; set; }
+    public Dictionary<string, Match> DisplayedMatches { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext db)
     {
@@ -21,9 +26,13 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        // !!! Think I did server side here but client side on the matches page, might refactor for consistency later
+        // !!! Ideally don't need to maintain two lists, probably come back to this
         MatchesByDefenseIds = new Dictionary<string, List<Match>>();
+        MatchStatsByIds = new Dictionary<string, Dictionary<string, float>>();
+        DisplayedMatches = new Dictionary<string, Match>();
 
-        Matches = _db.Match.ToList();
+        Matches = _db.Match.Where(m => m.CreatedAt >= DateTime.UtcNow.AddDays(withinDays)).ToList();
 
         foreach (var match in Matches)
         {
@@ -47,7 +56,7 @@ public class IndexModel : PageModel
 
             defenseIds.Sort();
 
-            var defenseKey = string.Join("-", defenseIds);
+            string defenseKey = string.Join("-", defenseIds);
 
             if (MatchesByDefenseIds.ContainsKey(defenseKey))
             {
@@ -56,6 +65,69 @@ public class IndexModel : PageModel
             else
             {
                 MatchesByDefenseIds.Add(defenseKey, new List<Match> { match });
+            }
+        }
+
+        foreach (var matchArray in MatchesByDefenseIds)
+        {
+            float wonCount = 0;
+            float lostCount = 0;
+            float totalMatches = 0;
+            float winRate;
+
+            foreach (var match in matchArray.Value)
+            {
+                if (match.Result == "lost")
+                {
+                    lostCount++;
+                }
+                else
+                {
+                    wonCount++;
+                }
+
+                totalMatches++;
+            }
+
+            winRate = (wonCount / totalMatches) * 100;
+
+            MatchStatsByIds.Add(matchArray.Key, new Dictionary<string, float>());
+            MatchStatsByIds[matchArray.Key].Add("won", wonCount);
+            MatchStatsByIds[matchArray.Key].Add("lost", lostCount);
+            MatchStatsByIds[matchArray.Key].Add("rate", winRate);
+        }
+
+        float currentLowestWinRate = 100;
+        Dictionary<string, float> DisplayedMatchesRateTracker = new Dictionary<string, float>();
+
+        foreach (var stats in MatchStatsByIds)
+        {
+            if (DisplayedMatches.Count() < maxMatchesToShow)
+            {
+                DisplayedMatches.Add(stats.Key, MatchesByDefenseIds[stats.Key].First());
+                DisplayedMatchesRateTracker.Add(stats.Key, stats.Value["rate"]);
+
+                if (stats.Value["rate"] <= currentLowestWinRate)
+                {
+                    currentLowestWinRate = stats.Value["rate"];
+                }
+            }
+            else
+            {
+                if (stats.Value["rate"] > currentLowestWinRate)
+                {
+                    DisplayedMatchesRateTracker = DisplayedMatchesRateTracker.OrderBy(key => key.Value)
+                        .ToDictionary(pair => pair.Key, pair => pair.Value);
+
+                    DisplayedMatches.Remove(DisplayedMatchesRateTracker.First().Key);
+                    DisplayedMatchesRateTracker.Remove(DisplayedMatchesRateTracker.Keys.First());
+
+                    DisplayedMatches.Add(stats.Key, MatchesByDefenseIds[stats.Key].First());
+                    DisplayedMatchesRateTracker.Add(stats.Key, stats.Value["rate"]);
+
+                    currentLowestWinRate = DisplayedMatchesRateTracker.OrderBy(key => key.Value)
+                        .ToDictionary(pair => pair.Key, pair => pair.Value).First().Value;
+                }
             }
         }
     }
